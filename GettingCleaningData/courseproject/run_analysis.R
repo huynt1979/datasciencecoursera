5b80dfdd0433aa0318ce9eae091072b0abf09bf3
Huy Nguyen <hnguyen@amd.home.net>
2015-09-20 22:05:49 -0700

    Fix script. Add output.txt file

--- GettingCleaningData/courseproject/run_analysis.R
+++ GettingCleaningData/courseproject/run_analysis.R
@@ -1,3 +1,6 @@
+# To execute this file, issue the following command:
+# Rscript run_analysis.R
+#
 library(plyr)
 
 checkFile <- function(fname) {
@@ -5,52 +8,56 @@ checkFile <- function(fname) {
     quit(save = "no", 0)
 }
 
-dataDir <- readline("Please enter path to data directory: ")
-print(dataDir)
+cat("Please enter path to data directory: ")
+conn <- file("stdin")
+dataDir <- readLines(conn, 1)
+close(conn)
+cat("You have entered the following path: ")
+cat(paste(dataDir, '\n'))
 
 if (!dir.exists(dataDir)) {
-  cat(dataDir, ' doesn\'t exist. Terminated!!!')
+  cat(dataDir, ' doesn\'t exist. Terminated!!!\n')
   quit(save = "no", 1)
 }
-setwd(dataDir)
-checkFile('features.txt')
-checkFile('activity_labels.txt')
-checkFile('train/subject_train.txt')
-checkFile('train/X_train.txt')
-checkFile('train/y_train.txt')
-checkFile('test/X_test.txt')
-checkFile('test/y_test.txt')
+#setwd(dataDir)
+checkFile(file.path(dataDir, 'features.txt'))
+checkFile(file.path(dataDir, 'activity_labels.txt'))
+checkFile(file.path(dataDir, 'train/subject_train.txt'))
+checkFile(file.path(dataDir, 'train/X_train.txt'))
+checkFile(file.path(dataDir, 'train/y_train.txt'))
+checkFile(file.path(dataDir, 'test/X_test.txt'))
+checkFile(file.path(dataDir, 'test/y_test.txt'))
 
-
+cat('Running...\n')
 # Read features
-features <- read.table('features.txt', header = FALSE)
+features <- read.table(file.path(dataDir, 'features.txt'), header = FALSE)
 names(features) <- c("FCode", "Feature")
 selectFeatureCols <- grep('mean\\(\\)|std\\(\\)', features$Feature)
 selectFeatureNames <- as.character(features[selectFeatureCols, "Feature"])
 
 # Read activities
-activities <- read.table('activity_labels.txt', header = FALSE)
+activities <- read.table(file.path(dataDir, 'activity_labels.txt'), header = FALSE)
 names(activities) <- c('ACode', 'Activity')
 
 # Read training data
-trainSubject <- read.table('train/subject_train.txt', header = FALSE)
+trainSubject <- read.table(file.path(dataDir, 'train/subject_train.txt'), header = FALSE)
 names(trainSubject) <- c('Subject')
-trainData <- read.table('train/X_train.txt', header = FALSE)
-trainLabel <- read.table('train/y_train.txt', header = FALSE)
+trainData <- read.table(file.path(dataDir, 'train/X_train.txt'), header = FALSE)
+trainLabel <- read.table(file.path(dataDir, 'train/y_train.txt'), header = FALSE)
 names(trainLabel) <- c('ACode')
 
 # Read testing data
-testSubject <- read.table('test/subject_test.txt', header = FALSE)
+testSubject <- read.table(file.path(dataDir, 'test/subject_test.txt'), header = FALSE)
 names(testSubject) <- c('Subject')
-testData <- read.table('test/X_test.txt', header = FALSE)
-testLabel <- read.table('test/y_test.txt', header = FALSE)
+testData <- read.table(file.path(dataDir, 'test/X_test.txt'), header = FALSE)
+testLabel <- read.table(file.path(dataDir, 'test/y_test.txt'), header = FALSE)
 names(testLabel) <- c('ACode')
 
 # 1. Merge training with testing data
 mergeData <- rbind(trainData, testData)
 
 # 2. Extract only mean and std features
-mergeData <- mergeData[ ,featureCols]
+mergeData <- mergeData[ ,selectFeatureCols]
 
 # 3. Name activities
 mergeLabel <- rbind(trainLabel, testLabel)
@@ -67,3 +74,7 @@ mergeSubject <- rbind(trainSubject, testSubject)
 subjectedData <- cbind(mergeSubject, activityData)
 subjectedData$ACode <- NULL
 avgSubjectedData <- ddply(subjectedData, .(Activity, Subject), colwise(mean), na.rm = TRUE)
+
+# Output as text file
+write.table(arrange(avgSubjectedData, Activity, Subject), file="output.txt", row.names = FALSE)
+cat("Output has been written to file output.txt\n")
